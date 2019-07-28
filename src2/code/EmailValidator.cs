45dd1b0f41d31da02c1e7c0d502d14d04a485546
Stu Sklinar <stuart.sklinar@alps.education>
2019-07-28 16:21:43 +0100

    init commit

---
+++ src2/code/EmailValidator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Valiation.Email
+{
+    public class EmailValidator
+    {
+        public bool Validate()
+        {
+            return false;
+        }
+    }
+}
---
+++ src2/tests/EmailValidatorTest.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using Valiation.Email;
+
+namespace Tests
+{
+    public class EmailValidatorTest
+    {
+        private EmailValidator _validator;
+
+        [SetUp]
+        public void Setup()
+        {
+            _validator = new EmailValidator();
+        }
+
+        [Test]
+        public void VaidateEmail()
+        {
+            _validator.Validate("");
+            Assert.Fail();
+        }
+    }
+}
